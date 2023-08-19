c9cc36daa8f12bac671f27da4580b106e307cfbf
Burak Tilek <burak.tilek@finceptiv.com>
2023-08-19 16:13:00 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+import { Root } from './components/Root'
+
+function App() {
+  return (
+    <Router basename='/app'>
+      <Routes>
+        <Route path='/' element={<Root />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/Root.tsx
@@ -0,0 +1,3 @@
+export const Root = () => {
+  return <>Well Hello There : )</>
+}
